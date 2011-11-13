b1552009047880c9c22c17c767b90676c2bcb54b
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-11-13 19:57:09 +0100

    add optional arguments before filename and page

--- pdf2fb.sh
+++ pdf2fb.sh
@@ -1,9 +1,12 @@
+#!/bin/sh -xe
+
 pdf=~/Downloads/pdf/NanoBSD_and_ALIX_BSD_06_2011.pdf
 pdf=/mnt/llin/rest/references/books/pdf/ggplot2-book.pdf
 page=$1
+shift
 
 test -z "$page" && page=1
 
-./pdfdraw -o page-%d.pgm -r 150 -b 1 -g -m $pdf $page
+./pdfdraw -o page-%d.pgm -r 150 -b 1 -g -m $* $pdf $page
 ./pgm2fb.pl page-$page.pgm | tee fb | nc 192.168.2.2 8888
 
