700ae1f02e67e48f6bdb40759d0a83a2150b3cc0
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-11-13 18:37:05 +0100

    support offset O_X and O_Y

--- pgm2fb.pl
+++ pgm2fb.pl
@@ -4,6 +4,9 @@ use strict;
 
 my $path = shift @ARGV || die "usage: $0 image.pgm";
 
+my $o_x = $ENV{O_X} || 0;
+my $o_y = $ENV{O_Y} || 0;
+
 open(my $in, '<', $path) || die "$path: $!";
 
 my $magic = <$in>;
@@ -17,11 +20,13 @@ my $fb;
 
 my $y = 0;
 
+seek($in, $w * $o_y, 1) if $o_y;
+
 while(my $size = read($in, my $px, $w)) {
 
 	foreach my $x ( 0 .. 824 / 2 - 1 ) {
 
-		my ($a,$b) = unpack('CC', substr($px,$x * 2,2));
+		my ($a,$b) = unpack('CC', substr($px,$x * 2 + $o_x,2));
 		print pack( 'C', (
 			( $a & 0xf0 )
 			|
