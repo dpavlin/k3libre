04072afb23aeadf5af11ec8c35708c6ebe77600a
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-05-18 19:15:10 +0200

    capture screenshot with correct orientation via usbnet ssh

---
+++ ssh-screenshot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -xe
+
+name=$1
+test -z "$name" && name=/tmp/kindle
+
+landscape=`ssh root@k cat /sys/module/eink_fb_hal_broads/parameters/bs_orientation`
+ssh root@k dd if=/dev/fb0 > $name.fb
+
+ls -al $name.fb
+
+./fb2pgm.pl $name.fb $landscape && display $name.fb.pgm
