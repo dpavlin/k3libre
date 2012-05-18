0cb20cf858e302a022f14cd4c499faf26bf97423
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-05-18 22:11:26 +0200

    use usbnet IP 192.168.2.2 for Kindle

--- ssh-screenshot.sh
+++ ssh-screenshot.sh
@@ -3,7 +3,7 @@
 name=$1
 test -z "$name" && name=/tmp/kindle
 
-landscape=`ssh root@k cat /sys/module/eink_fb_hal_broads/parameters/bs_orientation`
+landscape=`ssh root@192.168.2.2 cat /sys/module/eink_fb_hal_broads/parameters/bs_orientation`
 ssh root@k dd if=/dev/fb0 > $name.fb
 
 ls -al $name.fb
